06a172f97c8916c52192956bd27b95d45b764390
Slawomir Borowiec <przodownik@tlen.pl>
2016-04-06 15:22:25 +0200

    many2One

--- src/test/java/pl/java/scalatech/many2one/Many2OneJoinColumnTest.java
+++ src/test/java/pl/java/scalatech/many2one/Many2OneJoinColumnTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.ZonedDateTime;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,21 +15,36 @@ import lombok.extern.slf4j.Slf4j;
 import pl.java.scalatech.config.TestJpaConfig;
 import pl.java.scalatech.domain.Person;
 import pl.java.scalatech.domain.many2one.GroupPeople;
+import pl.java.scalatech.repository.PersonRepository;
 import pl.java.scalatech.repository.many2one.GroupPeopleRepo;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes={TestJpaConfig.class})
 public class Many2OneJoinColumnTest {
+    private Person p1;
+    private GroupPeople gp;
+    @Before
+   public  void init(){
+        p1 = Person.builder().firstname("slawek").email("slawek@tlen.pl").birthDay(ZonedDateTime.now()).build();
+        gp = new GroupPeople("javaForum", p1);
+    }
+
     @Autowired
     private GroupPeopleRepo groupPeopleRepo;
+    @Autowired
+    private PersonRepository personRepository;
 
     @Test
     public void shouldSave(){
-        Person p1 = Person.builder().firstname("slawek").email("slawek@tlen.pl").birthDay(ZonedDateTime.now()).build();
-        GroupPeople gp = new GroupPeople("javaForum", p1);
         groupPeopleRepo.save(gp);
-        log.info("gp : {}",gp);
+        log.info("gp : {}",groupPeopleRepo.findAll());
+    }
+
+   @Test
+    public void shouldLoadByPersonSite(){
+        groupPeopleRepo.save(gp);
+        log.info("person site : {}",personRepository.findAll());
     }
 
     @Test
