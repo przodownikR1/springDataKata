bda15270d8a641e25c06388647dd4b192dc4e7c6
Slawomir Borowiec <przodownik@tlen.pl>
2016-04-17 16:03:10 +0200

    test

--- src/test/java/pl/java/scalatech/example/CartTest.java
+++ src/test/java/pl/java/scalatech/example/CartTest.java
@@ -11,7 +11,6 @@ import org.javamoney.moneta.Money;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
