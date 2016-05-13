ef4089cf02bfa3d13cac5bbf67b71d8c5868929e
Slawomir Borowiec <przodownik@tlen.pl>
2016-05-13 10:56:11 +0200

    removed TODO test

--- src/main/java/pl/java/scalatech/repository/constructorResult/ConstructorResultRepo.java
+++
@@ -1,9 +0,0 @@
-package pl.java.scalatech.repository.constructorResult;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pl.java.scalatech.domain.constructorResult.Todo;
-
-public interface ConstructorResultRepo extends JpaRepository<Todo, Long>{
-
-}
